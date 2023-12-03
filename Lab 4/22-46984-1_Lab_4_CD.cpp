b379a04fbf37ed05a7ca372342d640d3de519edb
MD MOYNUL ISLAM <99502593+MOYNUL75@users.noreply.github.com>
2023-12-03 09:41:29 +0600

    Add files via upload

---
+++ Lab 4/22-46984-1_Lab_4_CD.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile("Program.cpp");
+    if (!inputFile.is_open()) {
+        cerr << "Error opening the file." << endl;
+        return 1;
+    }
+
+    unordered_set<string> functions;
+    unordered_map<string, int> operators;
+    regex functionDeclaration("([a-zA-Z_][a-zA-Z0-9_]*)\\s*\\([^\\)]*\\)\\s*\\{");
+
+    string line;
+
+    while (getline(inputFile, line)) {
+        smatch match;
+        if (regex_search(line, match, functionDeclaration)) {
+            // Extract function name from the matched group
+            string functionName = match[1];
+            functions.insert(functionName);
+        }
+
+        istringstream iss(line);
+        string word;
+
+        while (iss >> word) {
+            if (word == "+" || word == "-" || word == "*" || word == "=") {
+                // Count operators
+                operators[word]++;
+            }
+        }
+    }
+
+    // Output the results
+    int functionCount = 0;
+    cout << "User Defined Functions:" << endl;
+    for (const auto &function : functions) {
+        cout << "Function-" << ++functionCount << ": " << function << endl;
+    }
+    cout << "Total number of user-defined functions: " << functionCount << endl;
+
+
+    int operatorCount = 0;
+    cout << "\nOperators:" << endl;
+    for (const auto &op : operators) {
+        cout << "Operator-" << ++operatorCount << ": " << op.first << endl;
+    }
+
+    cout << "Total number of operators: " << operatorCount << endl;
+
+    inputFile.close();
+
+    return 0;
+}
